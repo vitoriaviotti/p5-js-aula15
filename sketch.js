3e5116b526d8f946b6a0872df405346cc9a691a2
vitoriaviotti <137062011+vitoriaviotti@users.noreply.github.com>
2023-06-23 08:54:01 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+let cor;
+let posicaoHorizontal;
+let posicaoVertical;
+function setup() {
+  createCanvas(400, 400);
+  background("white");
+}
+function draw() {
+  stroke("blue");
+  fill("red");
+  
+    if(mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 20);
+    }
+}
